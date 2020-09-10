8c723f0092ec5fd4370a4830fb3f1bbdfb1f1c81
Roberto Ffrench-Davis <roberto_ffrench@hotmail.com>
2020-09-10 10:30:05 -0300

    eliminar usuario listo

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -47,3 +47,31 @@ exports.putCustomer = (req, res, next) => {
         })
 
 }
+
+exports.deleteCustomer = (req, res, next) => {
+    const userId = req.params.userId; // se obtiene el ID de la URL dinamica /customers/:userId
+
+
+    // Se busca primero el usuario llamando al procedimiento almacenado de buscar usuario
+    sequelize.query('CALL getCustomer(:p_id)', { replacements: { p_id: userId } })
+        .then(row => {
+            if (row.length === 0) { // Si no encuentra un usuario la fila viene vacía
+                const error = new Error('Usuario no encontrado');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            // SI lo encontró el usuario se actualiza. query() devuelve una promesa por lo que se sigue anidando 
+            return sequelize.query('CALL deleteCustomer(:p_id)', { replacements: { p_id: userId } })
+        })
+        .then(result => {
+            console.log(result);
+            res.status(201).json({ resultado: 'Usuario Eliminado' }); // 201 es el codigo correcto en PUT
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
